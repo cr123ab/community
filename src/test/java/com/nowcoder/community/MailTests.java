2886565f141e280c2a6d25ce205b835858ec98e9
1176774657 <1176774657@qq.com>
2022-02-12 23:48:31 +0800

    后续提交 following commit

--- src/test/java/com/nowcoder/community/MailTests.java
+++ src/test/java/com/nowcoder/community/MailTests.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -16,8 +18,22 @@ public class MailTests {
     @Autowired
     private MailClient mailClient;
 
+    @Autowired
+    private TemplateEngine templateEngine;
+
     @Test
     public void testTextMail(){
         mailClient.sendMail("1176774657@qq.com","TEST","Welcome.");
+    }
+
+    @Test
+    public void testHtmlMail(){
+        Context context = new Context();
+        context.setVariable("username","sunday");
+
+        String content = templateEngine.process("/mail/demo",context);
+        System.out.println(content);
+
+        mailClient.sendMail("1176774657@qq.com","HTML",content);
     }
 }
