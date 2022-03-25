21d6659fef21943ea6ff25d76c95bcf8f14ea072
1176774657 <1176774657@qq.com>
2022-03-25 11:46:05 +0800

    后续提交 following commit

--- src/main/java/com/nowcoder/community/service/UserService.java
+++ src/main/java/com/nowcoder/community/service/UserService.java
@@ -173,4 +173,7 @@ public class UserService implements CommunityConstant {
         return userMapper.updateHeader(userId,headerUrl);
     }
 
+    public User findUserByName(String username){
+        return userMapper.selectByName(username);
+    }
 }
